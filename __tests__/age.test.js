ce9ed3c9fefc2a2373e16ad3841d1ca6403aeea4
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-02-04 10:30:41 -0800

    rename files and add GalacticAge export

---
+++ __tests__/age.test.js
@@ -0,0 +1,10 @@
+import { GalacticAge } from '../src/js/age.js';
+
+describe('GalacticAge', () => {
+
+  let input;
+
+  beforeEach(() => {
+    input = new GalacticAge(40, 80)
+  });
+});
---
+++ src/js/age.js
@@ -0,0 +1,10 @@
+export class GalacticAge {
+  constructor(age, lifeExpectency) {
+    this.age = age;
+    this.lifeExpectency = lifeExpectency;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+  }
+}
