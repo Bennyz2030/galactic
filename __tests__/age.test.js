a1d2b9b5d25b03c9aa83445445f94e57d5c889f0
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-02-04 10:33:52 -0800

    pass red phase for mercury age

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -7,4 +7,11 @@ describe('GalacticAge', () => {
   beforeEach(() => {
     input = new GalacticAge(40, 80)
   });
+
+  test('should correctly determine the users age on Mercury', () => {
+    input.mercury();
+    expect(input.mercuryAge).toEqual(9.6);
+  });
+
+  
 });
