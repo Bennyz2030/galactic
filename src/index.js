139a26428fb72205c7268af276566b4d287c23bb
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-02-04 15:31:43 -0800

    WIP building UI

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {GalacticAge} from './js/age.js';
 
-$;
+(document).ready(function() {
+  $("form#user-age").submit(function(event) {
+    event.preventDefualt();
+    let age = parseInt($("#age-input").val());
+    let lifeExpectency = parseInt($("#expectency-input").val());
+
+    let runAge = new GalacticAge(age, lifeExpectency);
+    runAge.mercury();
+    runAge.mercuryExpectency();
+
+    $('#output').number(runAge);
+  });
+})
